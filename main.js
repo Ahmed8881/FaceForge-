7df449841aabad43de50308b61f537f83b346e22
Ahmed8881 <buttahmed2004@gmail.com>
2025-05-31 23:36:19 +0500

    Implement FaceSyncAI class with video and canvas setup for face detection

---
+++ main.js
@@ -0,0 +1,18 @@
+  class FaceSyncAI {
+            constructor() {
+                this.video = document.getElementById('video');
+                this.canvas = document.getElementById('canvas');
+                this.ctx = this.canvas.getContext('2d');
+                this.startBtn = document.getElementById('startBtn');
+                this.status = document.getElementById('status');
+                this.spinner = document.getElementById('spinner');
+                this.particleOverlay = document.getElementById('particleOverlay');
+                
+                this.isStreaming = false;
+                this.currentFilter = 'normal';
+                this.faceBoxes = [];
+                this.particles = [];
+                
+                this.init();
+            }
+        }
